cccfc0bd7937e4614d8d20fad651b87d5aa6018b
Aleksa Ilić <aleksa.d.ilic@gmail.com>
2020-05-12 18:33:47 +0200

    Licensing and screenshot

--- wmenu/main.cpp
+++ wmenu/main.cpp
@@ -1,4 +1,12 @@
-﻿#include "wmenu.hpp"
+﻿/*
+	This Source Code Form is subject to the terms of the Mozilla Public
+	License, v. 2.0. If a copy of the MPL was not distributed with this
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+	Copyright (C) 2020 Aleksa Ilic <aleksa.d.ilic@gmail.com>
+*/
+
+#include "wmenu.hpp"
 
 #ifdef _WIN32
 #include <windows.h>
